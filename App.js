894ebf3c0994c413d4fd1b67147dfa55619b782b
Spencer Jones <spencerjones@fnni.com>
2023-08-22 12:41:01 -0500

    Add status bar styling

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StatusBar, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 function AccountsView({ navigation }) {
+  const insets = useSafeAreaInsets()
   return (
-    <View style={styles}>
+    <View style={[styles.container, {
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right
+      },
+    ]}>
+      <StatusBar barStyle="light-content" />
       <Text>Accounts View</Text>
       <Button 
       title="Go to Summary"
@@ -25,17 +34,37 @@ function AccountSummaryView() {
 const Stack = createNativeStackNavigator()
 
 export default function App() {
+  StatusBar.setBarStyle('dark-content', true)
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Accounts'>
-        <Stack.Screen name="Accounts" component={AccountsView} options={{headerLargeTitle: true}} />
-        <Stack.Screen name="Summary" component={AccountSummaryView} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <SafeAreaProvider>
+        
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Accounts'>
+            <Stack.Screen name="Accounts" component={AccountsView} options={navigationOptions} />
+            <Stack.Screen name="Summary" component={AccountSummaryView} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+      
+    
   );
 }
 
+const navigationOptions = {
+  title: "Accounts",
+  headerLargeTitle: true,
+  headerStyle: {
+    backgroundColor: '#006341'
+  },
+  headerTintColor: '#fff'
+}
+
+
 const styles = StyleSheet.create({
+  navigationHeader: {
+    color: 'white',
+    
+  },
   container: {
     flex: 1,
     alignItems: 'center',
