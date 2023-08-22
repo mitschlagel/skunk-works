beaae12275f078f25e6ed8f159c0471783543086
Spencer Jones <spencerjones@fnni.com>
2023-08-22 12:07:21 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-function HomeView() {
+function AccountsView({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home View</Text>
+    <View style={styles}>
+      <Text>Accounts View</Text>
+      <Button 
+      title="Go to Summary"
+      onPress={() => navigation.navigate('Summary')}
+      />
+    </View>
+  )
+}
+
+function AccountSummaryView() {
+  return (
+    <View style={styles}>
+      <Text>Account Summary View</Text>
     </View>
   )
 }
@@ -16,8 +27,9 @@ const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeView} />
+      <Stack.Navigator initialRouteName='Accounts'>
+        <Stack.Screen name="Accounts" component={AccountsView} options={{headerLargeTitle: true}} />
+        <Stack.Screen name="Summary" component={AccountSummaryView} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -26,7 +38,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
